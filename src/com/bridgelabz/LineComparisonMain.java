e219def82d5e81f4c6ceefa97945da4413163d92
iaksharhate <iaksharhate@gmail.com>
2022-04-23 13:08:46 +0530

    UC1: Calculate lenght of line.

---
+++ src/com/bridgelabz/LineComparisonMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class LineComparisonMain {
+    public static void main(String[] args) {
+
+        LineComparisonOops lineComparisonOops = new LineComparisonOops();
+        lineComparisonOops.calculateLineLength();
+    }
+}
---
+++ src/com/bridgelabz/LineComparisonOops.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LineComparisonOops {
+
+    public double calculateLineLength(){
+        int x1, x2, y1, y2;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter x1 value: ");
+        x1 = sc.nextInt();
+        System.out.println("Enter x2 value: ");
+        x2 = sc.nextInt();
+        System.out.println("Enter y1 value: ");
+        y1 = sc.nextInt();
+        System.out.println("Enter y2 value: ");
+        y2 = sc.nextInt();
+
+        double length = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length of the line between points " + "(" + x1 + "," + y1 + ")," +
+                "(" + x2 + "," + y2 + ") = " + length);
+        return length;
+    }
+}
